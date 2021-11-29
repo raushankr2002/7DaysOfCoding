8b0072e56cb97b72cc25f072d294a4857b595e73
RAUSHAN KUMAR <90882981+raushankr2002@users.noreply.github.com>
2021-11-29 11:35:00 +0530

    Add files via upload

---
+++ Day1.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	//cout<<"RauShan Kumar"<< endl;
+	
+	/*int a = 5;
+	cout<< a << endl;
+	
+	char ch = 'a';
+	cout<< ch<< endl;
+	
+	bool b = true;
+	cout<< b << endl;
+	
+	float f = 1.2;
+	cout<< f << endl;
+	
+	double d = 1.23;
+	cout<< d << endl;
+	*/
+	
+	/*int a = 5;
+	cout<< sizeof(a)<< endl;
+	
+	char ch = 'a';
+	cout<< sizeof(ch)<< endl;
+	
+	bool b = true;
+	cout<< sizeof(b)<< endl;
+	
+	float f = 1.2;
+	cout<< sizeof(f)<< endl;
+	
+	double d = 1.23;
+	cout<< sizeof(d) << endl;
+	*/
+	
+	/*float f =1.23;
+	int x =f;
+	cout<< x << endl;
+	*/
+	
+	//unsigned int x = -1;
+	//cout<< x << endl;
+	
+	
+	/*int x1 = 10;
+	unsigned int x2 = 10;
+	cout << x1 << endl;
+	cout<< x2 << endl;
+	
+	int x3 = 10;
+	unsigned int x4 = -10; //negative value can't store in unsigned data types if we try to assign the it will return big value.
+	cout << x3 << endl;
+	cout<< x4 << endl;
+	*/
+	
+	
+	
+	
+	
+	
+	
+	
+}
